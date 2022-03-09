7ce7b63d6f861d35082fb39e601bd0ca61c0f4b5
anandgitacc <anand.akmail@gmail.com>
2022-03-09 14:40:45 +0530

    firstgit

---
+++ src/org/client/Client.java
@@ -0,0 +1,13 @@
+package org.client;
+
+public class Client {
+	
+	public void clientName() {
+		System.out.println("Client name : CTS");
+	}
+	
+	public void clientLocation() {
+		System.out.println("Chennai");
+	}
+
+}
---
+++ src/org/employee/Company.java
@@ -0,0 +1,15 @@
+package org.employee;
+
+public class Company {
+	
+	public void comName() {
+		
+		System.out.println("Company name : Greens");
+	}
+	
+	public void comId() {
+		
+		System.out.println("company id : 1231");
+	}
+
+}
